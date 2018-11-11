9c89f307f9c3d0a00bc881d872c4fdb2fabbec48
Joe van der Zwet <me@joezwet.me>
2018-11-11 20:09:23 +1300

    Fix Item Meta
    Signed-off-by: Joe van der Zwet <me@joezwet.me>

--- src/main/java/com/fallenflame/fallenutil/inventories/items/GeneralItems.java
+++ src/main/java/com/fallenflame/fallenutil/inventories/items/GeneralItems.java
@@ -3,6 +3,7 @@ package com.fallenflame.fallenutil.inventories.items;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 /**
  * @Author Joe van der Zwet (JoeZwet#6252)
@@ -11,38 +12,50 @@ public class GeneralItems {
 
     public static ItemStack getCloseItem() {
         ItemStack itemStack = new ItemStack(Material.BARRIER);
-        itemStack.getItemMeta().setDisplayName(String.format("%s%sClose", ChatColor.RED, ChatColor.BOLD));
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(String.format("%s%sClose", ChatColor.RED, ChatColor.BOLD));
+        itemStack.setItemMeta(itemMeta);
         return itemStack;
     }
 
     public static ItemStack getFillerItem() {
         ItemStack itemStack = new ItemStack(Material.STAINED_GLASS_PANE, (short)1, (short)15);
-        itemStack.getItemMeta().setDisplayName("");
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName("");
+        itemStack.setItemMeta(itemMeta);
         return itemStack;
     }
 
     public static ItemStack getBackItem() {
         ItemStack itemStack = new ItemStack(Material.STAINED_GLASS_PANE, (short)1, (short)14);
-        itemStack.getItemMeta().setDisplayName(String.format("%s%sBack", ChatColor.RED, ChatColor.BOLD));
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(String.format("%s%sBack", ChatColor.RED, ChatColor.BOLD));
+        itemStack.setItemMeta(itemMeta);
         return itemStack;
     }
 
     public static ItemStack getAcceptItem() {
         ItemStack itemStack = new ItemStack(Material.STAINED_CLAY, (short)1, (short)5);
-        itemStack.getItemMeta().setDisplayName(String.format("%s%sAccept", ChatColor.GREEN, ChatColor.BOLD));
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(String.format("%s%sAccept", ChatColor.GREEN, ChatColor.BOLD));
+        itemStack.setItemMeta(itemMeta);
         return  itemStack;
     }
 
     public static ItemStack getDenyItem() {
         ItemStack itemStack = new ItemStack(Material.STAINED_CLAY, (short)1, (short)14);
-        itemStack.getItemMeta().setDisplayName(String.format("%s%sDeny", ChatColor.RED, ChatColor.BOLD));
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(String.format("%s%sDeny", ChatColor.RED, ChatColor.BOLD));
+        itemStack.setItemMeta(itemMeta);
         return  itemStack;
     }
 
     public static ItemStack getMessageItem(String message) {
-        ItemStack infoItem = new ItemStack(Material.NAME_TAG);
-        infoItem.getItemMeta().setDisplayName(String.format("%s%s%s", ChatColor.GRAY, ChatColor.BOLD, message));
-        return infoItem;
+        ItemStack itemStack = new ItemStack(Material.NAME_TAG);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(String.format("%s%s%s", ChatColor.GRAY, ChatColor.BOLD, message));
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
     }
 
 }
